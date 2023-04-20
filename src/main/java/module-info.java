20ecb5c10348bc7c79ddb3e913da775d190be92a
d3shira <shirarandobrava@live.com>
2023-04-20 21:23:19 +0200

    dbconnection+packages and classes

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,14 @@
 module com.example.projekti_knk {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens com.example.projekti_knk to javafx.fxml;
     exports com.example.projekti_knk;
+
+    exports service to javafx.graphics;
+    exports modules to javafx.graphics;
+    exports repository to javafx.graphics;
+    exports controller to javafx.graphics;
 }
---
+++ src/main/java/modules/User.java
@@ -0,0 +1,50 @@
+package modules;
+
+public class User {
+    private int id;
+    private String username;
+    private String saltedPassword;
+    private String salt;
+
+    public User(int id, String username, String saltedPassword, String salt) {
+        this.id = id;
+        this.username = username;
+        this.saltedPassword = saltedPassword;
+        this.salt = salt;
+    }
+
+    //GETTERS
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getSaltedPassword() {
+        return saltedPassword;
+    }
+
+    public String getSalt() {
+        return salt;
+    }
+
+    //SETTERS
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setSaltedPassword(String saltedPassword) {
+        this.saltedPassword = saltedPassword;
+    }
+
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
+}
