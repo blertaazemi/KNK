309f7f1c4e45fa29ee7a4d04ea74902122e615ff
d3shira <shirarandobrava@live.com>
2023-04-27 17:24:16 +0200

    shume gjuhesite + ne butona
    duhet te pershtaten id-te e butonave per login me ato ne projektin tone.
    Dafina B: logincontroller check it

--- src/main/java/controller/App.java
+++ src/main/java/controller/App.java
@@ -6,11 +6,18 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Locale;
 
 
 public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+
+        //set default locALE TO ENGLISH ose ndonje gjuhe tjeter
+
+        Locale.setDefault(new Locale("en","KS"));
+        Locale.setDefault(new Locale("de","KS"));
+
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("main.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Aplikimi per Bursa");
--- src/main/java/controller/LogInController.java
+++ src/main/java/controller/LogInController.java
@@ -1,4 +1,62 @@
 package controller;
 
-public class LogInController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import models.User;
+import service.UserAuthService;
+
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class LogInController  implements Initializable {
+    @FXML
+    private TextField txtUsername;
+    @FXML
+    private PasswordField pwdPassword;
+
+    @FXML
+    private Button btnLogin;
+
+
+    @FXML
+    private void loginClick(ActionEvent e){
+        String username = this.txtUsername.getText();
+        String password = this.pwdPassword.getText();
+
+        try{
+            User user = UserAuthService.login(username,password);
+            if(user==null){
+                //mesazh
+                System.out.println("Username or password is incorrect! ");
+                return;
+            }
+            System.out.println("User is correct!");
+        }catch(SQLException sqlException){
+            System.out.println("Gabim ne baze te te dhenave!!");
+        }
+
+        //System.out.printf("Username: %s, Password: %s", username, password);
+    }
+    @FXML
+    private void cancelClick(ActionEvent e){
+        this.txtUsername.setText("");
+        this.pwdPassword.setText("");
+    }
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        System.out.println("Initialize");
+
+        Locale locale=Locale.getDefault();
+        ResourceBundle translate=ResourceBundle.getBundle("translations.content",locale);
+        translate.getString("Login.button.text");
+        this.btnLogin.setText(translate.getString("Login.button.text"));
+    }
 }
