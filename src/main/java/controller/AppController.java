4e47b2ed2f26afe472f39ee6d9ba00c4bc333a1a
daf1na <dafinabalaj345@gmail.com>
2023-04-28 21:48:33 +0200

    first image

--- src/main/java/controller/AppController.java
+++ src/main/java/controller/AppController.java
@@ -1,11 +1,22 @@
 package controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
-public class AppController {
-    @FXML
-    private ImageView image1;
+import java.net.URL;
+import java.util.ResourceBundle;
 
+public class AppController implements Initializable {
+    @FXML
+    private ImageView imageView;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        Image image = new Image(getClass().getResourceAsStream("/Images/Rektorati_-_Universiteti_i_Prishtines.jpg"));
+        this.imageView.setImage(image);
+    }
 }
