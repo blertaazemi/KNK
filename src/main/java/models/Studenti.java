9fa8226d676406963f685e51930a8fe5ac26be56
bl3rt4 <blerta.aazemi@gmail.com>
2023-05-19 23:27:13 +0300

    .

--- src/main/java/models/Studenti.java
+++ src/main/java/models/Studenti.java
@@ -1,30 +1,34 @@
 package models;
 
 public class Studenti {
-    private String name;
-    private String username;
+    private String first_name;
+    private String last_name;
     private String email;
     private String gender;
     private String faculty;
 
 
+
     public Studenti(String name, String username, String email, String gender,String faculty) {
 
-        this.name=name;
-        this.username = username;
+        this.first_name=first_name;
+        this.last_name=last_name;
         this.email=email;
         this.gender=gender;
         this.faculty=faculty;
     }
 
-    //GETTERS
-
-    public String getname() {
-        return name;
+    public Studenti(int id, String first_name) {
     }
 
-    public String getusername() {
-        return username;
+    //GETTERS
+
+    public String getfirst_name() {
+        return first_name;
+    }
+
+    public String getlast_name() {
+        return last_name;
     }
 
     public String getemail() {
@@ -39,19 +43,19 @@ public class Studenti {
 
 
     //SETTERS
-    public void setname(String username) {
-        this.name = name;
+    public void setfirst_name(String first_name) {
+        this.first_name = first_name;
     }
 
-    public void setusername(String username) {
-        this.username = username;
+    public void setlast_name(String last_name) {
+        this.last_name = last_name;
     }
 
-    public void setemail(String username) {
+    public void setemail(String email) {
         this.email = email;
     }
-    public void setUsername(String username) {
-        this.username = username;
+    public void setgender(String gender) {
+        this.gender = gender;
     }
     public void setfaculty(String faculty) {
         this.faculty = faculty;
