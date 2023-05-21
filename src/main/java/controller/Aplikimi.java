5527ca2c761125c3a5f3e193ed6c9fd3ab4e003d
dafiinaa <dafinaasadiku@gmail.com>
2023-05-21 11:36:55 +0200

    .

--- src/main/java/controller/Aplikimi.java
+++ src/main/java/controller/Aplikimi.java
@@ -1,13 +1,10 @@
 package controller;
 
-<<<<<<< Updated upstream
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
 public class Aplikimi extends Application {
@@ -18,42 +15,7 @@ public class Aplikimi extends Application {
         AnchorPane root = loader.load();
         Scene scene = new Scene(root, 650, 564);
         stage.setTitle("Aplikimii Controller");
-=======
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
 
-import java.io.IOException;
-import java.util.Locale;
-
-
-public class Aplikimi extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-
-        //set default locALE TO ENGLISH ose ndonje gjuhe tjeter
-
-        Locale.setDefault(new Locale("en","KS"));
-        Locale.setDefault(new Locale("de","KS"));
-
-        FXMLLoader fxmlLoader = new FXMLLoader(Aplikimi.class.getResource("aplikimi.fxml"));
-
-        Scene scene = new Scene(fxmlLoader.load(), 650, 564);
-        stage.setTitle("Sistemi Interaktiv për aplikimin dhe menaxhimin e bursave të studentëve\n");
->>>>>>> Stashed changes
-        stage.setScene(scene);
-        stage.show();
     }
-
-<<<<<<< Updated upstream
-    public static void main(String[] args) {
-        launch(args);
-    }
-
 }
 
-=======
-
-}
->>>>>>> Stashed changes
