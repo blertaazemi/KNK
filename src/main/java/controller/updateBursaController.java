045b9bc9f96d5dade63271a60a0565be2b15739a
Besmira75 <besmira.berisha129@gmail.com>
2023-05-29 02:04:04 +0200

    t

--- src/main/java/controller/updateBursaController.java
+++ src/main/java/controller/updateBursaController.java
@@ -1,16 +1,24 @@
 package controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.SplitPane;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 import models.Bursat;
 import repository.BursatRepository;
 
-import java.awt.event.ActionEvent;
+import java.io.IOException;
+import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 public class updateBursaController {
     @FXML
@@ -23,7 +31,7 @@ public class updateBursaController {
     private AnchorPane sidebar;
 
     @FXML
-    private Button homeButton;
+    private Button homeButton2;
 
     @FXML
     private Button studentetButton;
@@ -32,11 +40,10 @@ public class updateBursaController {
     private Button bursa;
 
     @FXML
-    private Button aplikimetbtn;
+    private Button aplikimetbtn1;
 
     @FXML
     private AnchorPane updateBursa1;
-
 
     @FXML
     private TextField id;
@@ -46,6 +53,7 @@ public class updateBursaController {
 
     @FXML
     private TextField nota_mesatare;
+
     @FXML
     private TextArea description;
 
@@ -53,55 +61,94 @@ public class updateBursaController {
     private TextField amount;
 
     @FXML
-    public Button ubBtn;
+    private Button ubBtn;
+
     public int Id;
-
     public String Name;
-
     public Double Nota_mesatare;
-
     public String Description;
-
     public Double Amount;
+    @FXML
+    private Button translateAL;
+    @FXML
+    private Button translateEN;
 
+    private ResourceBundle bundle;
+    private Connection connection;
+
+
+    private void translateElements() {
+
+        Locale locale = Locale.getDefault();
+        ResourceBundle translate = ResourceBundle.getBundle("translations.content", locale);
+
+        homeButton2.setText(bundle.getString("homeButton2"));
+        studentetButton.setText(bundle.getString("studentetButton"));
+        bursa.setText(bundle.getString("bursabtn"));
+        aplikimetbtn1.setText(bundle.getString("aplikimetbtn1"));
+        id.setText(bundle.getString("id"));
+        name.setText(bundle.getString("name"));
+        nota_mesatare.setText(bundle.getString("nota_mesatare"));
+        description.setText(bundle.getString("description"));
+        amount.setText(bundle.getString("amount"));
+        ubBtn.setText(bundle.getString("ubBtn"));
+
+
+    }
+
+
+    public void translateEn(ActionEvent event) {
+        Locale.setDefault(new Locale("en"));
+        bundle = ResourceBundle.getBundle("translations.content", Locale.getDefault());
+        this.translateElements();
+    }
+
+    public void translateAl(ActionEvent event) {
+        Locale.setDefault(new Locale("sq"));
+        bundle = ResourceBundle.getBundle("translations.content", Locale.getDefault());
+        this.translateElements();
+    }
 
 
     public void setBursaInfo(int id, String name, Double nota_mesatare, String description, Double amount) {
-        Id=id;
+        Id = id;
         Name = name;
         Nota_mesatare = nota_mesatare;
         Description = description;
         Amount = amount;
 
-
-        this.id.setText
-                (String.valueOf(Id));
+        this.id.setText(String.valueOf(Id));
         this.name.setText(Name);
         this.nota_mesatare.setText(String.valueOf(Nota_mesatare));
         this.description.setText(description);
         this.amount.setText(String.valueOf(Amount));
-
     }
 
-
-//    public void updateBursa(ActionEvent event) {
-//        Bursat bursat = new Bursat(Integer.parseInt(id.getText()), name.getText(), Double.parseDouble(nota_mesatare.getText()), description.getText(), Double.parseDouble(amount.getText()));
-//        try {
-//            BursatRepository.updateBursa(bursat);
-//            System.out.println("Bursa updated successfully");
-//        } catch (SQLException e) {
-//            throw new RuntimeException(e);
-//        }
-//    }
-
-
-    public void updateBursa(javafx.event.ActionEvent actionEvent) {
+    public void updateBursa(ActionEvent actionEvent) {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("bursat.fxml"));
         Bursat bursat = new Bursat(Integer.parseInt(id.getText()), name.getText(), Double.parseDouble(nota_mesatare.getText()), description.getText(), Double.parseDouble(amount.getText()));
         try {
             BursatRepository.updateBursa(bursat);
             System.out.println("Bursa updated successfully");
+
+            // Retrieve the current stage
+            Stage currentStage = (Stage) ubBtn.getScene().getWindow();
+
+            // Load the new scene
+            Parent root = loader.load();
+
+            // Create a new stage for the new scene
+            Stage newStage = new Stage();
+            newStage.setScene(new Scene(root));
+
+            // Close the current stage and show the new stage
+            currentStage.close();
+            newStage.show();
         } catch (SQLException e) {
             throw new RuntimeException(e);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
-    }
+
+}
